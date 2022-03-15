527294550146c0ae96f821863d920fe5483ac86b
leiTos0 <ailton.leite2000@gmail.com>
2022-03-15 16:01:18 -0300

    exercicio 3

--- aula1/src/ex2/Ex2.java
+++ aula1/src/ex2/Ex2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ex2 {
 
 	public static void main(String[] args) {
-Scanner teclado = new Scanner(System.in);
+		Scanner teclado = new Scanner(System.in);
 		
 		System.out.println("Informe o salario: ");
 		double salario = teclado.nextDouble();
---
+++ aula1/src/ex3/Ex3.java
@@ -0,0 +1,15 @@
+package ex3;
+
+import java.util.Scanner;
+
+public class Ex3 {
+	public static void main(String[] args) {
+		System.out.println("Digite um numero: ");
+		Scanner teclado = new Scanner(System.in);
+		int n = teclado.nextInt();
+		for(int i = 1; i<=n; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
