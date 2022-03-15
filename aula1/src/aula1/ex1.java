0f5628b41932a07c2ee769d3c9e6ba86f4e00f59
leiTos0 <ailton.leite2000@gmail.com>
2022-03-15 14:33:45 -0300

    Exercicio 1

---
+++ aula1/src/aula1/ex1.java
@@ -0,0 +1,19 @@
+package aula1;
+
+import java.util.Scanner;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite a nota 1: ");
+		double n1 = teclado.nextInt();
+		System.out.println("Digite a nota 2: ");
+		double n2 = teclado.nextInt();
+		double media = (n1 + n2)/2;
+		System.out.println("A média do aluno é: "+media);
+				
+
+	}
+
+}
